a73a508e374f9f903c0a6d9d6ace76d24088d86b
Newton Manyisa <manyisanewton26@gmail.com>
2025-02-03 01:31:54 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+// Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    //function to handle onFocus when clicked
+    function handleFocus(){
+        console.log('Good!')
+    }
+    //function to handle OnBlur when clicked
+    function handleBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+  return (
+    <div>
+   <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from 'react'
+
+function Keypad() {
+    //function to handle onChange when clicked
+    function handleChange(){
+        console.log('Entering password...')
+    }
+  return (
+    <div>
+        <input type="password" onChange={handleChange} />
+
+    </div>
+  )
 }
 
-export default Keypad;
+export default Keypad
